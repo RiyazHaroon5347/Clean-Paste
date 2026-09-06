3e792f8d8cb0f689665a7f61934bfd681212177e
Mohamed Riyaz M <dafy.dev.int1@gmail.com>
2026-09-06 17:18:07 +0530

    feat: initialize project with React, Vite, and Tailwind CSS configuration

--- cleanpaste/vite.config.js
+++ cleanpaste/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Use '/' for a custom domain, or '/clean-paste/' if using username.github.io/clean-paste
+  plugins: [react(), tailwindcss()],
+  base: '/',
 })
+
